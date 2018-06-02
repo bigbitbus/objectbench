46e5997fbc364bad072bc58a6e062df8791636b5
Sachin Agarwal <sachin@bigbitbus.com>
2018-06-02 11:47:43 -0400

    Reduced numebers for testing

--- datamaker.py
+++ datamaker.py
@@ -3,9 +3,9 @@ from exercizer import Exercizer
 
 ex = Exercizer()
 
-ex.makeRandomBinFiles(numFiles = 25, minSizekb = 1, 
+ex.makeRandomBinFiles(numFiles = 2, minSizekb = 1, 
     maxSizekb = 101, outDir = sys.argv[1] )
-ex.makeRandomBinFiles(numFiles = 20, minSizekb = 200, 
+ex.makeRandomBinFiles(numFiles = 2, minSizekb = 200, 
     maxSizekb = 10001, outDir = sys.argv[1])
-ex.makeRandomBinFiles(numFiles = 10, minSizekb = 20000, 
+ex.makeRandomBinFiles(numFiles = 0, minSizekb = 20000, 
     maxSizekb = 100001, outDir = sys.argv[1])
