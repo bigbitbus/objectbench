dfa3fd9268ee262a7068da1c98aa8c76cff46368
Sachin Agarwal <sachin@bigbitbus.com>
2018-06-02 23:35:23 -0400

    Added env_credentials into the aws

--- awsexercizer.py
+++ awsexercizer.py
@@ -21,7 +21,7 @@ from traceback import format_exc
 from pprint import pprint
 import pickle
 class AWSExercizer(Exercizer):
-    def __init__(self, region_name ='us-east-1'):
+    def __init__(self, env_credentials= {}, region_name ='us-east-1'):
         Exercizer.__init__(self)
         if region_name == 'us-east-1': # aws quirk - you can't specify us-east-1 explicitly
             self.storage_client = boto3.client('s3',
@@ -79,7 +79,7 @@ class AWSExercizer(Exercizer):
         dic_downloadData['_listObjectsInContainer'] = (self.endTimer(), "Container objects listing time") 
         for aBlob in blobList:
             self.startTimer()
-            localPath = join(localDir,aBlob['Key'].split('/')[-1])
+            localPath = join(localDir,aBlob['Key'].split('/'))
             self.storage_client.download_file(container_name, aBlob['Key'], localPath)
             dic_downloadData[localPath] = (self.endTimer(), getsize(localPath))
         return dic_downloadData
@@ -101,7 +101,7 @@ if __name__=="__main__":
         'account': 'S3KEY', 
         'secret':'S3SECRET'
     }
-    awsex = AWSExercizer()
+    awsex = AWSExercizer(env_credentials = env_credentials)
     # Upload
  #   print "Upload objects"
  #   pprint(awsex.UploadObjectsToContainer())
